0eb3f3e873216f72ca5c83c9e5d11e7b33d668b3
JunSeo2001 <kanu6233@naver.com>
2025-10-04 16:13:15 +0900

    Initial commit - 3주차까지 완료

---
+++ app/src/main/java/com/example/flo_clone_app/data/Album.kt
@@ -0,0 +1,10 @@
+package com.example.flo_clone_app.data
+
+// build.gradle(Module): plugins { id("kotlin-parcelize") } 필요
+
+
+data class Album(
+    val title: String = "",
+    val singer : String = ""
+)
+
---
+++ app/src/main/java/com/example/flo_clone_app/data/Song.kt
@@ -0,0 +1,6 @@
+package com.example.flo_clone_app.data
+
+data class Song(
+    val title: String = "",
+    val singer : String = ""
+)
